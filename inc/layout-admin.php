a21b0beef45735b45d7b84452f023b3b19eed375
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-05-19 08:54:13 +0000

    layout manager clean-up
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/themes/sem-reloaded/trunk@1339 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- inc/layout-admin.php
+++ inc/layout-admin.php
@@ -110,7 +110,7 @@ EOS;
 		foreach ( array(
 			'wrapper' => __('Canvas', 'sem-reloaded'),
 			'content' => __('Content', 'sem-reloaded'),
-			'wide_sidebars' => __('Wide Sidebars', 'sem-reloaded'),
+			'wide_sidebars' => __('Wide Sidebar', 'sem-reloaded'),
 			'sidebars' => __('Sidebars', 'sem-reloaded'),
 			'inline_boxes' => __('Inline Boxes', 'sem-reloaded'),
 			) as $key => $detail ) {
@@ -210,242 +210,86 @@ EOS;
 	function get_layouts() {
 		return array(
 			'mts' => array(
-				'name' => __('Content, Wide Sidebars + 2 Sidebars', 'sem-reloaded'),
+				'name' => __('Content, Wide Sidebar', 'sem-reloaded'),
 				'wrapper' => __('950px', 'sem-reloaded'),
-				'content' => __('550px (520px)', 'sem-reloaded'),
-				'wide_sidebars' => __('400px (370px)', 'sem-reloaded'),
-				'sidebars' => __('200px (170px)', 'sem-reloaded'),
-				'inline_boxes' => __('4 x 237px (207px)', 'sem-reloaded'),
+				'content' => __('550px (520px net)', 'sem-reloaded'),
+				'wide_sidebars' => __('400px (370px net)', 'sem-reloaded'),
+				'sidebars' => __('200px (170px net)', 'sem-reloaded'),
+				'inline_boxes' => __('4 x 237px (207px net)', 'sem-reloaded'),
 				),
 			'sms' => array(
 				'name' => __('Sidebar, Content, Sidebar', 'sem-reloaded'),
 				'wrapper' => __('950px', 'sem-reloaded'),
-				'content' => __('550px (520px)', 'sem-reloaded'),
+				'content' => __('550px (520px net)', 'sem-reloaded'),
 				'wide_sidebars' => __('Unavailable', 'sem-reloaded'),
-				'sidebars' => __('200px (170px)', 'sem-reloaded'),
-				'inline_boxes' => __('4 x 237px (207px)', 'sem-reloaded'),
+				'sidebars' => __('200px (170px net)', 'sem-reloaded'),
+				'inline_boxes' => __('4 x 237px (207px net)', 'sem-reloaded'),
 				),
 			'mms' => array(
 				'name' => __('Wide Content, Sidebar', 'sem-reloaded'),
 				'wrapper' => __('950px', 'sem-reloaded'),
-				'content' => __('750px (720px)', 'sem-reloaded'),
+				'content' => __('750px (720px net)', 'sem-reloaded'),
 				'wide_sidebars' => __('Unavailable', 'sem-reloaded'),
-				'sidebars' => __('200px (170px)', 'sem-reloaded'),
-				'inline_boxes' => __('4 x 237px (207px)', 'sem-reloaded'),
+				'sidebars' => __('200px (170px net)', 'sem-reloaded'),
+				'inline_boxes' => __('4 x 237px (207px net)', 'sem-reloaded'),
 				),
 			'smm' => array(
 				'name' => __('Sidebar, Wide Content', 'sem-reloaded'),
 				'wrapper' => __('950px', 'sem-reloaded'),
-				'content' => __('750px (720px)', 'sem-reloaded'),
+				'content' => __('750px (720px net)', 'sem-reloaded'),
 				'wide_sidebars' => __('Unavailable', 'sem-reloaded'),
-				'sidebars' => __('200px (170px)', 'sem-reloaded'),
-				'inline_boxes' => __('4 x 237px (207px)', 'sem-reloaded'),
+				'sidebars' => __('200px (170px net)', 'sem-reloaded'),
+				'inline_boxes' => __('4 x 237px (207px net)', 'sem-reloaded'),
 				),
 			'ms' => array(
 				'name' => __('Content, Sidebar', 'sem-reloaded'),
 				'wrapper' => __('750px', 'sem-reloaded'),
-				'content' => __('550px (520px)', 'sem-reloaded'),
+				'content' => __('550px (520px net)', 'sem-reloaded'),
 				'wide_sidebars' => __('Unavailable', 'sem-reloaded'),
-				'sidebars' => __('200px (170px)', 'sem-reloaded'),
-				'inline_boxes' => __('3 x 250px (220px)', 'sem-reloaded'),
+				'sidebars' => __('200px (170px net)', 'sem-reloaded'),
+				'inline_boxes' => __('3 x 250px (220px net)', 'sem-reloaded'),
 				),
 			'sm' => array(
 				'name' => __('Sidebar, Content', 'sem-reloaded'),
 				'wrapper' => __('750px', 'sem-reloaded'),
-				'content' => __('550px (520px)', 'sem-reloaded'),
+				'content' => __('550px (520px net)', 'sem-reloaded'),
 				'wide_sidebars' => __('Unavailable', 'sem-reloaded'),
-				'sidebars' => __('200px (170px)', 'sem-reloaded'),
-				'inline_boxes' => __('3 x 250px (220px)', 'sem-reloaded'),
+				'sidebars' => __('200px (170px net)', 'sem-reloaded'),
+				'inline_boxes' => __('3 x 250px (220px net)', 'sem-reloaded'),
 				),
 			'mm' => array(
 				'name' => __('Wide Content', 'sem-reloaded'),
 				'wrapper' => __('750px', 'sem-reloaded'),
-				'content' => __('750px (720px)', 'sem-reloaded'),
+				'content' => __('750px (720px net)', 'sem-reloaded'),
 				'wide_sidebars' => __('Unavailable', 'sem-reloaded'),
 				'sidebars' => __('Unavailable', 'sem-reloaded'),
-				'inline_boxes' => __('3 x 250px (220px)', 'sem-reloaded'),
+				'inline_boxes' => __('3 x 250px (220px net)', 'sem-reloaded'),
 				),
 			'm' => array(
 				'name' => __('Narrow Content', 'sem-reloaded'),
 				'wrapper' => __('620px', 'sem-reloaded'),
-				'content' => __('620px (590px)', 'sem-reloaded'),
+				'content' => __('620px (590px net)', 'sem-reloaded'),
 				'wide_sidebars' => __('Unavailable', 'sem-reloaded'),
 				'sidebars' => __('Unavailable', 'sem-reloaded'),
-				'inline_boxes' => __('2 x 310px (290px)', 'sem-reloaded'),
+				'inline_boxes' => __('2 x 310px (290px net)', 'sem-reloaded'),
 				),
 			'mmm' => array(
 				'name' => __('Extra Wide Content', 'sem-reloaded'),
 				'wrapper' => __('950px', 'sem-reloaded'),
-				'content' => __('950px (920px)', 'sem-reloaded'),
+				'content' => __('950px (920px net)', 'sem-reloaded'),
 				'wide_sidebars' => __('Unavailable', 'sem-reloaded'),
 				'sidebars' => __('Unavailable', 'sem-reloaded'),
-				'inline_boxes' => __('4 x 237px (207px)', 'sem-reloaded'),
+				'inline_boxes' => __('4 x 237px (207px net)', 'sem-reloaded'),
 				),
 			'tsm' => array(
-				'name' => __('Wide Sidebars + 2 Sidebars, Content', 'sem-reloaded'),
+				'name' => __('Wide Sidebar, Content', 'sem-reloaded'),
 				'wrapper' => __('950px', 'sem-reloaded'),
-				'content' => __('550px (520px)', 'sem-reloaded'),
-				'wide_sidebars' => __('400px (370px)', 'sem-reloaded'),
-				'sidebars' => __('200px (170px)', 'sem-reloaded'),
-				'inline_boxes' => __('4 x 237px (207px)', 'sem-reloaded'),
+				'content' => __('550px (520px net)', 'sem-reloaded'),
+				'wide_sidebars' => __('400px (370px net)', 'sem-reloaded'),
+				'sidebars' => __('200px (170px net)', 'sem-reloaded'),
+				'inline_boxes' => __('4 x 237px (207px net)', 'sem-reloaded'),
 				),
 			);
 	} # get_layouts()
 } # sem_layout_admin
-
-
-class old_sem_layout_admin
-{
-	#
-	# init()
-	#
-	
-	function init()
-	{
-		add_action('admin_menu', array('sem_layout_admin', 'admin_menu'));
-	} # init()
-	
-	
-	#
-	# admin_menu()
-	#
-	
-	function admin_menu()
-	{
-		add_theme_page(
-			__('Layout'),
-			__('Layout'),
-			'switch_themes',
-			basename(__FILE__),
-			array('sem_layout_admin', 'admin_page')
-			);
-	} # admin_menu()
-	
-	
-	#
-	# update_options()
-	#
-	
-	function update_options()
-	{
-		check_admin_referer('sem_layout');
-
-		global $sem_options;
-
-		$sem_options['active_layout'] = preg_replace("/[^mest]/", "", $_POST['active_layout']);
-
-		update_option('sem6_options', $sem_options);
-	} # update_options()
-	
-	
-	#
-	# admin_page()
-	#
-	
-	function admin_page()
-	{
-		global $sem_options;
-
-		echo '<div class="wrap">' . "\n";
-		echo '<form method="post" action="">' . "\n";
-
-		if ( function_exists('wp_nonce_field') ) wp_nonce_field('sem_layout');
-
-		echo '<input type="hidden"'
-			. ' name="update_layout_options"'
-			. ' value="1"'
-			. ' />' . "\n";
-
-		if ( $_POST['update_layout_options'] )
-		{
-			sem_layout_admin::update_options();
-			
-			echo '<div class="updated">' . "\n"
-				. "<p>"
-					. "<strong>"
-					. __('Settings saved.')
-					. "</strong>"
-				. "</p>" . "\n"
-				. "</div>" . "\n";
-		}
-		
-		echo '<h2>' . __('Layout Settings') . '</h2>' . "\n";
-		
-		$layouts = sem_layout_admin::get_layouts();
-
-		if ( !in_array($sem_options['active_layout'], array_keys($layouts)) )
-		{
-			$sem_options['active_layout'] = 'mts';
-			update_option('sem6_options', $sem_options);
-		}
-		
-		$active_layout = $sem_options['active_layout'];
-		
-		$i = 0;
-		$last_id = key(array_reverse($layouts));
-		
-		echo '<table>' . "\n";
-		
-		foreach ( $layouts as $layout_id => $layout_data )
-		{
-			$i++;
-			
-			if ( $i % 2 )
-				echo '<tr align="center">' . "\n";
-			
-			echo '<td'
-				. ( ( $layout_id == $active_layout )
-					? ' style="width: 50%; background-color: #ddd;"'
-					: ' style="width: 50%;"'
-					)
-				. '>' . "\n";
-			
-			echo '<h3>'
-					. '<label for="active_layout__' . $layout_id . '">'
-					. '<input type="radio"'
-						. ' id="active_layout__' . $layout_id . '" name="active_layout"'
-						. ' value="' . $layout_id . '"'
-						. ( ( $layout_id == $active_layout )
-							? ' checked="checked"'
-							: ''
-							)
-						. ' />'
-					. '&nbsp;'
-					. $layout_data['name']
-					. '</label>'
-				. '</h3>' . "\n";
-
-			echo '<p>'
-				. '<label for="active_layout__' . $layout_id . '">'
-				. '<img src="' . sem_url . '/inc/img/' . $layout_id . '.png" />'
-				. '</label>'
-				. '</p>' . "\n";
-
-			echo '</td>' . "\n";
-			
-			if ( $i % 2 && $skin_id == $last_id ) {
-				echo '<td>&nbsp;</td>' . "\n";
-				$i++;
-			}
-			
-			if ( !( $i % 2 ) ) {
-				echo '</tr>' . "\n"
-					. '<tr>' . "\n"
-					. '<td colspan="2">'
-					. '<div class="submit">'
-					. '<input type="submit" value="' . attribute_escape(__('Save Changes')) . '" />'
-					. '</div>' . "\n"
-					. '</td>'
-					. '</tr>' . "\n";
-			}
-		}
-
-		echo '</table>' . "\n";
-		
-		echo '</form>' . "\n";
-		echo '</div>' . "\n";
-	} # admin_page()
-} # sem_layout_admin
-
-#sem_layout_admin::init();
 ?>
