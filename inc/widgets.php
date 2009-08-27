211c590a0170af92173922544c08740f240be4bd
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-08-27 09:41:20 +0000

    nav menus: fix sortable when using ajax
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/themes/sem-reloaded/trunk@2448 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- inc/widgets.php
+++ inc/widgets.php
@@ -2544,6 +2544,12 @@ class sem_nav_menu extends WP_Widget {
 			echo '<div class="nav_menu_item_blank">' . "\n"
 				. '<p>' . __('Empty Navigation Menu. Leave it empty to populate it automatically.', 'sem-reloaded') . '</p>' . "\n"
 				. '</div>' . "\n";
+		} elseif ( defined('DOING_AJAX') ) {
+			echo <<<EOS
+<script type="text/javascript">
+jQuery('div.nav_menu_item_sortables:has(.nav_menu_item)').sortable({});
+</script>
+EOS;
 		}
 		
 		echo '</div>' . "\n"; # sortables
