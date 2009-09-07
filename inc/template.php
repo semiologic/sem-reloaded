7f8dbe485e7d001e300ece5defba03f3f9b33f29
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-09-07 07:18:51 +0000

    simplify letter and print template customization
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/themes/sem-reloaded/trunk@2579 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- inc/template.php
+++ inc/template.php
@@ -138,15 +138,13 @@ class sem_template {
 			wp_enqueue_style('icons', sem_url . '/css/icons.css', null, sem_version);
 		
 		if ( isset($_GET['action']) && $_GET['action'] == 'print' ) {
+			wp_enqueue_style('skin', sem_url . '/css/print.css', null, sem_version);
 			if ( file_exists($skin_path . '/print.css') )
-				wp_enqueue_style('skin', $skin_url . '/print.css', null, sem_version);
-			else
-				wp_enqueue_style('skin', sem_url . '/css/print.css', null, sem_version);
+				wp_enqueue_style('custom', $skin_url . '/print.css', null, sem_version);
 		} elseif ( apply_filters('active_layout', $sem_options['active_layout']) == 'letter' ) {
+			wp_enqueue_style('skin', sem_url . '/css/letter.css', null, sem_version);
 			if ( file_exists($skin_dir . '/letter.css') )
-				wp_enqueue_style('skin', $skin_url . '/letter.css', null, sem_version);
-			else
-				wp_enqueue_style('skin', sem_url . '/css/letter.css', null, sem_version);
+				wp_enqueue_style('custom', $skin_url . '/letter.css', null, sem_version);
 		} else {
 			wp_enqueue_style('skin', $skin_url . '/skin.css', null, sem_version);
 			if ( file_exists(sem_path . '/custom.css') )
