ca22a66091760e3f0f6a7d0e818ac3400327daf9
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-10-03 16:45:30 +0000

    apply more... caption to fancy excerpt
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/themes/sem-reloaded/trunk@2938 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- inc/widgets.php
+++ inc/widgets.php
@@ -336,6 +336,9 @@ class entry_content extends WP_Widget {
 			);
 		
 		$this->WP_Widget('entry_content', $widget_name, $widget_ops, $control_ops);
+		
+		if ( class_exists('fancy_excerpt') )
+			add_filter('the_content_more_link', array(&$this, 'more_link'), 0);
 	} # entry_content()
 	
 	
@@ -460,6 +463,29 @@ class entry_content extends WP_Widget {
 				. $after_widget;
 		}
 	} # widget()
+	
+	
+	/**
+	 * more_link()
+	 *
+	 * @param string $more
+	 * @return string $more
+	 **/
+
+	function more_link($more) {
+		if ( !$this->number )
+			return $more;
+		
+		$instance = $this->get_settings();
+		$instance = $instance[$this->number];
+		$instance = wp_parse_args($instance, entry_content::defaults());
+		
+		$more = '<a href="' . apply_filters('the_permalink', get_permalink()) . '#more-' . get_the_ID() . '" class="more-link">'
+			. sprintf($instance['more_link'], get_the_title())
+			. '</a>';
+		
+		return $more;
+	} # more_link()
 	
 	
 	/**
