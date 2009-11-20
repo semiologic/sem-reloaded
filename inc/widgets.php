2a3b4298c7b564bc39cd04680692fb19893cbff2
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-11-20 10:56:07 +0000

    drop remaining calls to window.open
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/themes/sem-reloaded/trunk@3375 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- inc/widgets.php
+++ inc/widgets.php
@@ -2805,7 +2805,7 @@ class sem_nav_menu extends WP_Widget {
 				. '</div>' . "\n" # data
 				. '<div class="nav_menu_item_preview">' . "\n"
 				. '&rarr;&nbsp;<a href="' . esc_url($url) . '"'
-					. ' onclick="window.open(this.href); return false;">'
+					. ' target="_blank">'
 					. $label
 					. '</a>'
 				. '</div>' . "\n" # preview
@@ -2942,7 +2942,7 @@ EOS;
 				. '</div>' . "\n" # data
 				. '<div class="nav_menu_item_preview">' . "\n"
 				. '&rarr;&nbsp;<a href="' . esc_url($url) . '"'
-					. ' onclick="window.open(this.href); return false;">'
+					. ' target="_blank">'
 					. $label
 					. '</a>'
 				. '</div>' . "\n" # preview
